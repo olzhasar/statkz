96bfeb31ee762f0d774963cc208e1102a17b1e00
Olzhas <olzhas@localhost.localdomain>
2017-12-07 20:51:03 +0600

    Added charts logic

--- app/chart.py
+++ app/chart.py
@@ -2,12 +2,47 @@ import pandas as pd
 hdf_file = 'app/data.h5'
 hdf_key = 'social_demographic'
 
+colors = (
+    (),
+    (),
+    (),
+    (),
+    (),
+    (),
+)
+
 
 class Series():
-    def __init__(self, ):
-        pass
+    def __init__(self, key, legend, color=0, fill=True):
+        self.key = key
+        self.legend = legend
+        self.color = color
+        self.fill = fill
+        self.values = []
+
+    def backgroundColor(self):
+        return 'rgba({0},{1},{2},0.4)'.format(
+            colors[self.color][0],
+            colors[self.color][0],
+            colors[self.color][0]
+        )
+
+    def borderColor(self):
+        return 'rgba({0},{1},{2},1)'.format(
+            colors[self.color][0],
+            colors[self.color][0],
+            colors[self.color][0]
+        )
 
 
 class Chart():
-    def __init__(self):
-        pass
+    def __init__(self, title, series, kind='line', tooltip=None):
+        self.title = title
+        self.kind = kind
+        self.tooltip = tooltip
+        columns = [s.key for s in series]
+        data = pd.read_hdf(hdf_file, hdf_key)[columns].dropna()
+        self.labels = data.index.tolist()
+        for s in series:
+            s.values = data[s.key].tolist()
+        self.series = series
--- app/views/index.py
+++ app/views/index.py
@@ -1,10 +1,52 @@
 from flask import render_template
 from app import app
+from app.chart import Chart, Series
 
 
 @app.route('/')
 def index():
-    charts = []
+    charts = [
+        Chart(title='Среднедушевые номинальные доходы населения',
+              series=[
+                  Series(
+                      key='average_nominal_income_per_person_usd',
+                      legend='Долларов США',
+                      color=2,
+                      fill=True,
+                  )
+              ],
+              kind='bar'),
+        Chart(title='Средний размер ежемесячной пенсии',
+              series=[
+                  Series(
+                      key='average_monthly_pension_usd',
+                      legend='Долларов США',
+                      color=2,
+                      fill=True,
+                  )
+              ],
+              kind='bar'),
+        Chart(title='Количество больниц в РК',
+              series=[
+                  Series(
+                      key='hospitals_total',
+                      legend='ед.',
+                      color=0,
+                      fill=True,
+                  )
+              ],
+              kind='line'),
+        Chart(title='Количество школ в РК',
+              series=[
+                  Series(
+                      key='schools_total',
+                      legend='ед.',
+                      color=0,
+                      fill=True,
+                  )
+              ],
+              kind='line'),
+    ]
     return render_template('index.html', charts=charts)
 
 
